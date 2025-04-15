428767e71f476866be0138c9a6c86ba1cfb8b3eb
Raylight-Developer <Raylight.creator@gmail.com>
2025-04-14 18:40:03 -0600

    Fixed Input, Working History

--- Engine/Include/Input.hpp
+++ Engine/Include/Input.hpp
@@ -3,6 +3,7 @@
 #include "KL.hpp"
 
 class Input : public QObject {
+	QSet<int> keys_held;
 protected:
 	bool eventFilter(QObject *obj, QEvent *event) override;
 };
--- Engine/Source/Input.cpp
+++ Engine/Source/Input.cpp
@@ -5,16 +5,30 @@
 bool Input::eventFilter(QObject* obj, QEvent* event) {
 	if (event->type() == QEvent::KeyPress) {
 		QKeyEvent* key_event = static_cast<QKeyEvent*>(event);
-		if (key_event->modifiers() == Qt::KeyboardModifier::ControlModifier) {
-			if (key_event->key() == Qt::Key::Key_Z) {
-				H_UNDO(1);
-				return false;
-			}
-			else if (key_event->key() == Qt::Key::Key_Y) {
-				H_REDO(1);
-				return false;
+		if (key_event->isAutoRepeat()) {
+			return false;
+		}
+		const int key = key_event->key();
+		if (!keys_held.contains(key)) {
+			keys_held.insert(key);
+			if (key_event->modifiers() == Qt::KeyboardModifier::ControlModifier) {
+				if (key == Qt::Key::Key_Z) {
+					H_UNDO(1);
+					return false;
+				}
+				else if (key == Qt::Key::Key_Y) {
+					H_REDO(1);
+					return false;
+				}
 			}
 		}
+	}
+	else if (event->type() == QEvent::KeyRelease) {
+		QKeyEvent* key_event = static_cast<QKeyEvent*>(event);
+		if (key_event->isAutoRepeat()) {
+			return false;
+		}
+		keys_held.remove(key_event->key());
 	}
 
 	return QObject::eventFilter(obj, event);
